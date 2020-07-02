8844076436d002f9002c63f2e27dae70e7ada596
Daniel Gomez <danielgomez@MacBook-Pro.attlocal.net>
2020-07-02 14:04:11 -0500

    Visual Answer Sent to DOM

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const length = document.getElementById('length');
 const width = document.getElementById('width');
 const sqr_sides = document.getElementById('sqr_length');
+const answer_1 = document.getElementById('answer-label1');
+const answer_2 = document.getElementById('answer-label2');
 
 
 /* Perimeter for Rectangle is 2*(length + width) = P */
@@ -9,6 +11,7 @@ Rectangle = () => {
     let l = parseInt(length.value);
     let P = 2*(l + w);
 
+    answer_1.innerHTML = P;
     console.log(P);
 }
 
@@ -17,6 +20,6 @@ Square = () => {
     let a = parseInt(sqr_sides.value);
     let P = 4*(a);
 
+    answer_2.innerHTML = P;
     console.log(P)
-
 }
