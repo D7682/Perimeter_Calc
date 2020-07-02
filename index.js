ddc368e45418706ee84acde41d23f31576213138
Daniel Gomez <danielgomez@MacBook-Pro.attlocal.net>
2020-07-02 13:32:39 -0500

    Answers Are logged Into the console at the moment

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const length = document.getElementById('length');
 const width = document.getElementById('width');
+const sqr_sides = document.getElementById('sqr_length');
 
-change = () => {
+
+/* Perimeter for Rectangle is 2*(length + width) = P */
+Rectangle = () => {
     let w = parseInt(width.value);
     let l = parseInt(length.value);
     let P = 2*(l + w);
 
     console.log(P);
+}
+
+/* Perimeter for square is x+x+x+x = P or 4(x) = P */
+Square = () => {
+    let a = parseInt(sqr_sides.value);
+    let P = 4*(a);
+
+    console.log(P)
 
 }
